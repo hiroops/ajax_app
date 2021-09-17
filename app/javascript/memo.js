b02ac4625278e2259bc992e62a72579d7832db75
hiroops <89205167+hiroops@users.noreply.github.com>
2021-09-17 22:13:12 +0900

    credentials.yml.enc

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,5 +1,6 @@
 const buildHTML = (XHR) => {
  const item = XHR.response.post;
+  // console.log(item);
  const html =`
     <div class="post">
       <div class="post-date">
@@ -10,6 +11,7 @@ const buildHTML = (XHR) => {
       </div>
     </div>`;
   return html;
+  // console.log(html);
 };
 
 function post (){
@@ -17,20 +19,23 @@ function post (){
   submit.addEventListener("click", (e) => {
     e.preventDefault();
     const form = document.getElementById("form");
+    // console.log(form);
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
     XHR.onload = () => {
+      // console.log(formData);
       if (XHR.status != 200) {
         alert(`Error ${XHR.status}: ${XHR.statusText}`);
         return null;
       };
       const list = document.getElementById("list");
+      // console.log(list)
       const formText = document.getElementById("content");
-      // console.log(formText.value);
- 
+      // console.log(formText);
+      
       list.insertAdjacentHTML("afterend", buildHTML(XHR));
       formText.value ="";
     };
